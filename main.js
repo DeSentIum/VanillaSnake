4b0fa3a8b0b61c8e1c2f08ce0a44f9b147571f01
Jakub-Ozmina20 <jakub.ozmina20@gmail.com>
2023-07-03 15:12:39 +0200

    Refactored baseGame Class

--- main.js
+++ main.js
@@ -1,49 +1,26 @@
-// sleep time expects milliseconds
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-// Usage!
-
-
 class Game {
-
     constructor() {
         this.playGame = true;
         this.canvas = document.getElementsByClassName('game');
         this.ctx = this.canvas[0].getContext("2d");
     }
 
-    init() {
-        console.log("Hello World!");
-        this.loop();
+    sleep(time) {return new Promise((resolve) => setTimeout(resolve, time))}
 
+    init() {
+        this.loop();
         this.ctx.globalCompositeOperation = 'destination-under';
     }
 
     loop() {
-        document.addEventListener("keydown", (e) => {
-            if(e.code == "Escape") {
-                this.playGame = false;
-            }
-        });
+        document.addEventListener("keydown", (e) => e.code == "Escape" ? this.playGame = false : this.playGame = true);
         
-        if(this.playGame) {
-            sleep(60/1000).then(() => this.draw()).then(() => {this.loop()});
-        } else {
-            console.log("Exit");
-            this.abort();
-        }
+        if(this.playGame) this.sleep(60/1000).then(() => this.draw()).then(() => {this.loop()});
+        else this.abort();
     }
 
     draw() {
-        let randomRed = Math.floor(Math.random() * 255);
-        let randomBlue = Math.floor(Math.random() * 255);
-        let randomGreen = Math.floor(Math.random() * 255);
-        console.log("ddd");
-        this.ctx.fillStyle = "blue";
-        
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+       
     }
 
     abort() {
@@ -56,6 +33,8 @@ class Game {
     }
 
 }
+
+//setup
 document.addEventListener("DOMContentLoaded", () => {
     let game = new Game();
     game.init();
