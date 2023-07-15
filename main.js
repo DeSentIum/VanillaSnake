aafef40c7c695ec14db9fb7cefd141a4623ce041
Jakub-Ozmina20 <jakub.ozmina20@gmail.com>
2023-07-15 22:32:27 +0200

    Init

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
 class Game {
     constructor() {
         this.playGame = true;
-        this.canvas = document.getElementsByClassName('game');
-        this.ctx = this.canvas[0].getContext("2d");
+        this.canvas = document.getElementById('game');
+        this.velocity = [0, 0];
+        this.position = [0.0, 0.0];
+        this.deltaTime;
+        this.lastFrame;
     }
 
     sleep(time) {return new Promise((resolve) => setTimeout(resolve, time))}
 
     init() {
-        this.loop();
-        this.ctx.globalCompositeOperation = 'destination-under';
+        if(this.canvas.getContext) {
+            this.ctx = this.canvas.getContext("2d");
+            this.ctx.globalCompositeOperation = 'destination-over';   
+            this.lastFrame = 0;
+            window.requestAnimationFrame(this.loop());
+        }
     }
 
-    loop() {
+    update() {
+        // Update the state of the world for the elapsed time since last render
         document.addEventListener("keydown", (e) => e.code == "Escape" ? this.playGame = false : this.playGame = true);
-        
-        if(this.playGame) this.sleep(60/1000).then(() => this.draw()).then(() => {this.loop()});
-        else this.abort();
+        document.addEventListener("keydown", (e) => {
+            for(let i = 0; i < 2; i++) {
+                this.velocity[i] = 0.0;
+            }
+            if(e.code == "KeyD") {
+                
+                this.velocity[0] = 1.0;
+            }
+            if(e.code == "KeyW") {
+                this.velocity[1] = 1.0;
+            }
+            if(e.code == "KeyS") {
+                this.velocity[1] = -1.0;
+            }
+            if(e.code == "KeyA") {
+                this.velocity[0] = -1.0;
+            }
+            console.log({"Position: ": this.position, "Velocity: ": this.velocity});
+            
+        });
+        console.log("test");
+        for(let i = 0; i < 2; i++) {
+            this.position[i] += this.velocity[i];
+        }
     }
-
+      
     draw() {
-       
+        this.ctx.fillStyle = "rgb(200, 0, 0)";
+        
+        this.ctx.fillRect(this.position[0], this.position[1], 10, 10);
+    }
+      
+    loop(timestamp = 0) {
+        this.deltaTime = timestamp - this.lastRender;
+      
+        this.update();
+        this.draw();
+      
+        this.lastFrame = timestamp;
+        if(this.playGame) window.requestAnimationFrame(this.loop());
+        else this.abort();
     }
 
     abort() {
@@ -31,7 +73,6 @@ class Game {
             }
         });
     }
-
 }
 
 //setup
